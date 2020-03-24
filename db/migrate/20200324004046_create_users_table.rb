f84585de8e10ca88443e42f5e1bec8150a142eae
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-03-24 00:42:17 +0000

    Automatically backed up by Learn

--- db/migrate/20200324004046_create_users_table.rb
+++ db/migrate/20200324004046_create_users_table.rb
@@ -1,5 +1,13 @@
 class CreateUsersTable < ActiveRecord::Migration[5.1]
   def change
-    
+    class Users < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.decimal :balance
+    end
+  end
+end
   end
 end
